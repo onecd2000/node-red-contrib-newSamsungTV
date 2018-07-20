98acd5771461c76ac04b36c4583dfe8c596edf7b
yahya atwady <onecd2000@gmail.com>
2018-07-20 15:03:24 +0300

    is alive node

---
+++ README.md
@@ -0,0 +1,7 @@
+# node-red-contrib-newSamsungTV
+## Node for controling new Models of Samsung Smart TV (Tizen Smart Hub).
+The code based on new samsung remote library by [Andrei Stanciu](https://github.com/andrei10k).
+The code also based on the greate work of [Fred Thx](https://github.com/FredThx).
+I hope you find this node helpful. Thank you.
+
+**Tested on Samsung 7 Series (65) (UA65MU7000)**
--- newSamsungTV.js
+++ newSamsungTV.js
@@ -2,8 +2,48 @@ var samsungRemote = require("new-samsung-remote");
 
 
 
-module.exports = function(RED) {
+// module.exports = function(RED) {
     
+//     function newSamsungTV(config) {
+//         const node_config = {
+//             ip_address: config.ip, // required: IP address of your Samsung Smart TV
+//             name: "New Samsung Remote"
+//         };
+//         RED.nodes.createNode(this, config);
+// 		this.ip = config.ip;
+// 		this.command = config.command;
+// 		this.name = config.name;
+// 		this.remote = new samsungRemote(node_config);
+// 		var node = this;
+		
+//         this.on('input', function (msg) {
+            
+//             var command = node.command || msg.command;			
+// 			if(!command){
+//                 msg.success = false;
+//                 msg.err = "no command defined for samsungTV!";
+//                 node.warn(msg.err);
+// 				node.send(msg);
+//             }else{
+//                 node.remote.api_active();            
+// 				node.remote.sendKey(command, function (err, res) {
+// 					if (err) {
+// 						throw new Error(err);
+// 					} else {
+// 						msg.success = true;
+// 					}
+// 					node.send(res);
+// 				});
+//             }
+            
+//         });
+//     }
+
+//     RED.nodes.registerType("newSamsungTV", newSamsungTV);
+// }
+
+function registerNewSamsungTvNodes(RED) {
+
     function newSamsungTV(config) {
         const node_config = {
             ip_address: config.ip, // required: IP address of your Samsung Smart TV
@@ -40,4 +80,36 @@ module.exports = function(RED) {
     }
 
     RED.nodes.registerType("newSamsungTV", newSamsungTV);
+
+/* ---------------------------------------------------------------------------
+   * ISALIVE node
+   * -------------------------------------------------------------------------*/
+  function NewSamsungTvNodeIsAlive(config) {
+    RED.nodes.createNode(this, config);
+    const node_config = {
+        ip_address: config.ip, // required: IP address of your Samsung Smart TV
+        name: "New Samsung Remote"
+    };
+    RED.nodes.createNode(this, config);
+    this.ip = config.ip;
+    this.name = config.name;
+    this.remote = new samsungRemote(node_config);
+    var node = this;
+
+    
+
+      node.on('input', function(msg) {
+        node.remote.api_active(function (err) {
+          if (err) {
+            node.send([null, msg]);
+          } else {
+            node.send([msg, null]);
+          }
+        });
+      });
+
+  
+  }
+  RED.nodes.registerType("new-samsung-tv-isalive", NewSamsungTvNodeIsAlive);
 }
+module.exports = registerNewSamsungTvNodes;
